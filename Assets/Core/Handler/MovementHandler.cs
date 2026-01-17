eb6bc889991f58ce7a6854c8591184e702e22641
Aryan <aryan@MacBook-Air-de-Aryan.local>
2026-01-17 19:44:14 +0100

    sprite anim

--- Assets/Core/Handler/MovementHandler.cs
+++ Assets/Core/Handler/MovementHandler.cs
@@ -2,20 +2,25 @@ using System.Runtime.Serialization.Formatters;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class MovementHandler: MonoBehaviour
+public class MovementHandler : MonoBehaviour
 {
     public float moveSpeed = 8f;
     public float jumpForce = 14f;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     private Rigidbody2D rb;
     private Collider2D col;
-    public bool isGrounded { get; private set;}
+    public bool isGrounded { get; private set; }
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>;
+
         // rb.transform.position = new Vector3(0, -1, 0);
     }
 
@@ -27,23 +32,23 @@ public class MovementHandler: MonoBehaviour
             return;
         }
 
-
         if (!(input.moveY < -0.5f)) {
-        rb.linearVelocity = new Vector2(input.moveX * moveSpeed, rb.linearVelocity.y);
-        self.CoordX = rb.transform.position.x;
+            rb.linearVelocity = new Vector2(input.moveX * moveSpeed, rb.linearVelocity.y);
+            self.CoordX = rb.transform.position.x;
+            animator.SetBool("isWalking", (input.moveX > 0.5f || input.moveX < -0.5f));
         }
-        if (input.jump && isGrounded)
-        {
+
+        if (input.jump && isGrounded) {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
+
         if (input.moveX > 0.5f && self.facing == PlayerComponent.Direction.Left) {
             // back
             self.isBlocking = true;
         } else if (input.moveX < -0.5f && self.facing == PlayerComponent.Direction.Right) {
             // other back
             self.isBlocking = true;
-        }
-        else {
+        } else {
             self.isBlocking = false;
         }
         if (input.moveY < -0.5f) {
@@ -55,23 +60,23 @@ public class MovementHandler: MonoBehaviour
             //col.offset = new Vector2(col.offset.x, 0f);
             //col.transform.localScale = new Vector3(1f, 1f, 1f);
         }
-
-        
-
+        if (self.facing == PlayerComponent.Direction.Left) {
+            spriteRenderer.flipX = true;
+        } else if (self.facing == PlayerComponent.Direction.Right) {
+            spriteRenderer.flipX = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Ground"))
-        {
+        if (collision.collider.CompareTag("Ground")) {
             isGrounded = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Ground"))
-        {
+        if (collision.collider.CompareTag("Ground")) {
             isGrounded = false;
         }
     }
